5be4b50625345b92d888991b1664ed47b5e32002
isgandarisgandarov <isgenderov0202@gmail.com>
2021-04-08 15:55:34 +0400

    Amazon Scraper

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -2,46 +2,51 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-product = 'playstation 4'
-url = f'https://amazon.com/s?k={product}'
 
-chromeOptions = Options()
-chromeOptions.headless = True
-driver = webdriver.Chrome(executable_path='C:\\Webdriver\\bin\\chromedriver.exe', options=chromeOptions)
-driver.get(url)
+def scraper(product):
+    if product == '':
+        return []
+    url = f'https://amazon.com/s?k={product}'
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-results = soup.find_all('div', {'data-component-type': 's-search-result'})
+    chromeOptions = Options()
+    chromeOptions.headless = True
+    driver = webdriver.Chrome(executable_path='C:\\Webdriver\\bin\\chromedriver.exe', options=chromeOptions)
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    results = soup.find_all('div', {'data-component-type': 's-search-result'})
+
+    def scrape_info(item):
+        try:
+            atag = item.h2.a
+            title = atag.text.strip()
+            link = 'https://amazon.com' + atag.get('href')
+        except AttributeError:
+            title = "No title provided"
+        try:
+            price_parent = item.find('span', 'a-price')
+            price = price_parent.find('span', 'a-offscreen').text.strip()
+        except AttributeError:
+            price = "No price provided"
+        try:
+            rating = item.i.text
+        except AttributeError:
+            rating = "No rating provided"
+
+        result = {'title': title,
+                  'price': price,
+                  'rating': rating,
+                  'link': link,
+                  'source': 'amazon.com'}
+        return result
 
 
-def scrape_info(item):
-    try:
-        atag = item.h2.a
-        title = atag.text.strip()
-        link = 'https://amazon.com' + atag.get('href')
-    except AttributeError:
-        title = "No title provided"
-    try:
-        price_parent = item.find('span', 'a-price')
-        price = price_parent.find('span', 'a-offscreen').text.strip()
-    except AttributeError:
-        price = "No price provided"
-    try:
-        rating = item.i.text
-    except AttributeError:
-        rating = "No rating provided"
+    products = []
+    for item in results:
+        record = scrape_info(item)
+        if record:
+            products.append(record)
 
-    result = {'title': title,
-              'price': price,
-              'rating': rating,
-              'link': link}
-    return result
+    driver.quit()
+    return products
 
-
-products = []
-for item in results:
-    record = scrape_info(item)
-    if record:
-        products.append(record)
-
-driver.quit()
--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, url_for
-from amazon_scraper import products
-
+from flask import Flask, render_template, url_for, request
+from amazon_scraper import scraper
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '34806b7e050652f3fc730b3fbc22d0ee'
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', products=products)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        item = request.form['search']
+        products = scraper(item)
+        return render_template('index.html', products=products)
+    else:
+        products = []
+        return render_template('index.html', products=products)
 
 
 if __name__ == '__main__':
